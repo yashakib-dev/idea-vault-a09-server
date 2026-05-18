c66bd966ebba6c151b8aab2d8de75f0e9b6f5f03
yashakib-dev <yashakib.dev@gmail.com>
2026-05-19 00:42:07 +0600

    Backend created and connected to mongodb

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config()
+const uri = process.env.MONGODB_URI;
+const app = express()
+const PORT = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db('ideaVault')
+    const ideaCollection = db.collection('idea')
+
+    app.post('/idea', async(req, res)=>{
+      const ideaData = req.body
+      const result = await ideaCollection.insertOne(ideaData)
+      res.json(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
